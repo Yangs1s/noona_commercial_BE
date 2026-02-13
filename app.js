5e421ceb41465cece858d3b8bf7e9194179af720
YangS1s <sungjinahim@gmail.com>
2026-02-13 21:28:02 +0900

    aws 세팅

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ app.use(cors());
 app.use("/api", indexRouter);
 
 const LOCAL_DB_ADDRESS = process.env.LOCAL_DB_ADDRESS;
+const AWS_DB_ADDRESS = process.env.AWS_DB_ADDRESS;
 mongoose
-  .connect(LOCAL_DB_ADDRESS)
+  .connect(AWS_DB_ADDRESS)
   .then(() => {
     console.log("Connected to MongoDB");
   })
