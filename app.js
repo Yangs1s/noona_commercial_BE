e472e56b2b5ebaa05df37f4264e025ce40e37ed4
YangS1s <sungjinahim@gmail.com>
2026-02-19 11:45:00 +0900

    Pagination 추가

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ app.use("/api", indexRouter);
 
 const LOCAL_DB_ADDRESS = process.env.LOCAL_DB_ADDRESS;
 const AWS_DB_ADDRESS = process.env.AWS_DB_ADDRESS;
-const DB_ADDRESS =
-  process.env.NODE_ENV === "production" ? AWS_DB_ADDRESS : LOCAL_DB_ADDRESS;
+
 mongoose
-  .connect(DB_ADDRESS)
+  .connect(LOCAL_DB_ADDRESS)
   .then(() => {
-    console.log("DB_ADDRESS", DB_ADDRESS);
     console.log("Connected to MongoDB");
   })
   .catch((err) => {
     console.log("DB Connection Error: ", err);
   });
 
+console.log("AWS_DB_ADDRESS:", process.env.AWS_DB_ADDRESS);
+console.log("LOCAL_DB_ADDRESS:", process.env.LOCAL_DB_ADDRESS);
 app.listen(process.env.PORT || 5001, () => {
   console.log(`Server is running on port ${process.env.PORT || 5001}`);
 });
--- controller/product.controller.js
+++ controller/product.controller.js
@@ -31,6 +31,7 @@ productController.getProducts = async (req, res) => {
       limit = 10,
       sort = "createdAt",
       order = "desc",
+      query = "",
     } = req.query;
     const maxLimit = Math.min(limit, 50);
 
@@ -40,10 +41,21 @@ productController.getProducts = async (req, res) => {
         .json({ status: "한 페이지당 최대 50개의 상품만 조회할 수 있습니다." });
     }
 
+    const cond = query ? { name: { $regex: query, $options: "i" } } : {};
+    const totalCount = await Product.countDocuments(cond);
     const skip = page ? (page - 1) * limit : 0;
-    const totalCount = await Product.countDocuments();
-    const products = await Product.find().skip(skip).limit(limit);
+    let queryBuilder = Product.find(cond);
+    queryBuilder = queryBuilder
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sort]: order === "asc" ? 1 : -1 });
 
+    // const products = await Product.find(keyword)
+    //   .skip(skip)
+    //   .limit(limit)
+    //   .sort({ [sort]: order === "asc" ? 1 : -1 });
+
+    const products = await queryBuilder.exec();
     return res.status(200).json({
       status: "상품 조회 성공",
       data: products,
@@ -52,7 +64,7 @@ productController.getProducts = async (req, res) => {
       currentPage: page,
       itemsPerPage: limit,
     });
-  } catch {
+  } catch (error) {
     res.status(400).json({ status: "상품 조회 실패", error: error.message });
   }
 };
